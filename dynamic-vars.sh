ab75bff1e2c23d24913bd9bdd022739181464501
unknown <devopsanil.team>
2023-03-17 19:16:54 +0530

    auto push

--- dynamic-vars.sh
+++ dynamic-vars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 TODAYDATE=$(date +%F)
 echo -e "todays date is \e[32m ${TODAYDATE} \e[0m"
-echo -e "number of user session in the system are : $(who | wc -l)"
+echo -e "number of user session in the system are :\e[32m $(who | wc -l)\e[0m"
