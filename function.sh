a746662a1dabdbdcd7b9062af28e7ef0317bd350
unknown <devopsanil.team>
2023-03-17 21:42:25 +0530

    auto push

--- function.sh
+++ function.sh
@@ -3,7 +3,7 @@
  stat() {
  echo " total number of sessions : $(who | wc -l)"
  echo "todays date is : $(date +%F)"
- echo " load average on the system is $(uptime | awk -F : '(print $NF)' | awk -F , '(print $1)')"
+ echo " load average on the system is $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1}')"
  } 
  echo "calling stat function " 
  stat
