8b7602e5eebe769d83da46ec2e99975b6783ee03
unknown <devopsanil.team>
2023-03-17 16:50:23 +0530

    auto push

---
+++ colours.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+# Colors       Foreground          Background
+
+# Red               31                  41
+
+# Green             32                  42
+
+# Yellow            33                  43
+
+# Blue              34                  44
+
+# Magenta           35                  45
+
+# Cyan              36                  46
+ 
+# the command to print colours is 
+echo -e "\e[32m green is one of the fav colour of my friend \e[0m"
