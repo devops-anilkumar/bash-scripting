21ea4492ee05ad0f1cb585a491dc40ba8e2de834
unknown <devopsanil.team>
2023-03-17 23:44:55 +0530

    auto push

--- esac.sh
+++ esac.sh
@@ -7,12 +7,15 @@ ACTION=$1
 case $ACTION in
         start)
              echo "starting service"
+             exit0
              ;;
         stop )
               echo "stoping service"
+              exit2
               ;;
         restart)
                echo "restarting service"
+               exit3
              ;;
         *)
              echo "valid options are start or stop or restart"
