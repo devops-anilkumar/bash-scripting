4a9ec83ef2760b7d0405b3cc14ac758c6162bd93
unknown <devopsanil.team>
2023-03-17 20:15:00 +0530

    auto push

--- read.sh
+++ read.sh
@@ -1,11 +1,3 @@
-#!/bin/bash  
-
-
-
-
-
-
-
-
-read -p "enter the user running the script : "name
-echo -e  "name of the user running the script is $NAME "
+#!/bin/bash
+read -p"enter the user running the script : "
+echo -e "name of the user running the script is $name "
