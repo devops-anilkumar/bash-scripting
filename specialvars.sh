d9f02f4a0bba635ef958261018713935930ae7e2
unknown <devopsanil.team>
2023-03-17 19:43:50 +0530

    auto push

--- specialvars.sh
+++ specialvars.sh
@@ -4,11 +4,13 @@
 #$1 : takes the 1st value 
 #$2 : takes the  second  value 
 #$3 : takes the third value 
-#$* : is going to print  the  used variables 
+#$* or $@
+
+ : is going to print  the  used variables 
 # $# ; is going to print the total number of variables 
 echo "name of the script is : $0"
 echo "first value is $1"
 echo "second value is $2"
 echo "third value is $3"
 echo "total number of supplied variables are : $# "
- echo " total number of used variables : $* "
+ echo " total number of used variables : $@ "
