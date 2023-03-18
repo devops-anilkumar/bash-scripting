7d5149eddbdfa5f734e523987a015d586a3fe2ff
unknown <devopsanil.team>
2023-03-19 00:32:57 +0530

    auto push

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -14,29 +14,42 @@ if [ "$ID" -ne 0 ] ; then
   echo -e "\e[31m you should excute this script as root user or with sudo as prefix \e[0m"
   exit 1
 fi 
-echo -n " INSTALLING NGINX :"
-if [ $? -eq 0 ] ;then 
+stat() {
+if [ $1 -eq 0 ] ;then 
  echo -e "\e[32m success\e[0m" 
 else
  echo -e "\e[31m failure\e[0m" 
  exit2
 fi 
+}
 
+echo "INSTALLING NGINX:"
 yum install nginx -y     &>> $LOGFILE
+stat $?
+
+
+
+echo -n "DOWNLOADING THE $COMPONENT component:"
 curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+stat $?
 
 
 
-
-
+echo -n "PERFORMING CLEANUP OF OLD CONTENT:"
 cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
+rm -rf *   &>> $LOGFILE
+stat $?
+
+
+echo -n "COPYING THE DOWNLOADED $COMPONENT CONTENT:"
+unzip /tmp/frontend.zip   &>> $LOGFILE
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
 
-
-systemctl enable nginx
-systemctl restart nginx
+echo -n "STARTING THE SERVICE:"
+systemctl enable nginx  &>> $LOGFILE
+systemctl restart nginx &>> $LOGFILE
+stat $?
