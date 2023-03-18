a65414d58696505617b92a98aad0e8137011472e
unknown <devopsanil.team>
2023-03-18 11:37:40 +0530

    auto push

--- ifelse.sh
+++ ifelse.sh
@@ -9,6 +9,6 @@ elif [ "$ACTION" == "stop" ]; then
       echo -e "\e[31m service payment is starting \e[0m"
       exit o   
 else
-      echo -e "\e[33m service payment is starting \e[0m"
+      echo -e "\e[33m service payment is unknown \e[0m"
       exit 3
 fi
