3113e0ad953aca0f1fffd114edb14495ba0b175d
unknown <devopsanil.team>
2023-03-19 22:17:07 +0530

    auto push

---
+++ robot/components/rabbitmq.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+COMPONENT=rabbitmq
+source components/common.sh
+
+echo -n "INSTALLING AND CONFIGURING DEPENDANCY :"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+stat $?
+
+echo -n "INSTALLING $COMPONENT :"
+yum install rabbitmq-server -y  &>> $LOGFILE
+stat $?
+
+echo -n "STARTING THE $COMPONENT :"
+systemctl enable rabbitmq-server   &>> $LOGFILE
+systemctl start rabbitmq-server     &>> $LOGFILE
+stat $?
+
+echo -n "CREATING $COMPONENT APPLICATION USER :"
+rabbitmqctl add_user roboshop roboshop123  &>> $LOGFILE
+stat $? 
+
