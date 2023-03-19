f95534f40b1987e757c5e26f1f09f089476e712e
unknown <devopsanil.team>
2023-03-19 22:19:09 +0530

    auto push

--- robot/components/rabbitmq.sh
+++ robot/components/rabbitmq.sh
@@ -4,8 +4,8 @@ COMPONENT=rabbitmq
 source components/common.sh
 
 echo -n "INSTALLING AND CONFIGURING DEPENDANCY :"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash               &>> $LOGFILE
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash      &>> $LOGFILE
 stat $?
 
 echo -n "INSTALLING $COMPONENT :"
