0c34207283be90de6b8a10a231a27f2237346acf
unknown <devopsanil.team>
2023-03-19 22:56:32 +0530

    auto push

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
+echo " i am catalogue"   
 
 
 
-
-
-
-echo " i am catalogue"
 COMPONENT=catalogue
-source components/common.sh # source is going to load the file ,so that you can call oll of them as per your need
-NODEJS       # calling nodejs function
+
+
+source components/common.sh       # source is going to load the file ,so that you can call oll of them as per your need
+NODEJS                            # calling nodejs function
 
 #source is acommand to import and run the file 
 
